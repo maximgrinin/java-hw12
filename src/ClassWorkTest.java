2e4f31b3a675a14a34cb1e357f8e44f991512988
maximgrinin <maximgrinin@gmail.com>
2022-12-02 18:03:32 +0300

    Initial Commit

---
+++ src/ClassWorkTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassWorkTest {
+    @Test
+    public void calcSum() {
+    }
+
+    @Test
+    public void calcDiff() {
+    }
+
+    @Test
+    public void calcMult() {
+    }
+
+    @Test
+    public void calcDiv() {
+    }
+
+}
